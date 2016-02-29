4021b803b6038936d8a939805f97b3e087b236aa
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-02-29 10:18:19 +0100

    Adds namespacing for ENV. Removes error for none found

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
 'use strict'
 
 var config = {
-  JWT_KEY: process.env.JWT_KEY || 'NeverShareYourSecret',
-  CALLBACK_STATUS_MESSAGE: process.env.CALLBACK_STATUS_MESSAGE || 'Til behandling',
-  JOB_DIRECTORY_PATH: process.env.JOB_DIRECTORY_PATH || 'test/data/jobs',
-  QUEUE_NEXT_URL: process.env.QUEUE_NEXT_URL || 'https://example.com/api/queue/next',
-  QUEUE_DELETE_URL: process.env.QUEUE_DELETE_URL || 'https://example.com/api/queue'
+  JWT_KEY: process.env.TFK_SQP_JWT_KEY || 'NeverShareYourSecret',
+  CALLBACK_STATUS_MESSAGE: process.env.TFK_SQP_CALLBACK_STATUS_MESSAGE || 'Til behandling',
+  JOB_DIRECTORY_PATH: process.env.TFK_SQP_JOB_DIRECTORY_PATH || 'test/data/jobs',
+  QUEUE_NEXT_URL: process.env.TFK_SQP_QUEUE_NEXT_URL || 'https://example.com/api/queue/next',
+  QUEUE_DELETE_URL: process.env.TFK_SQP_QUEUE_DELETE_URL || 'https://example.com/api/queue'
 }
 
 module.exports = config
